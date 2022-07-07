80a659c4871ad544ffffea8a4fc145c937add6d9
lgdd <louis-guillaume.durand@liferay.com>
2022-07-07 17:02:39 +0200

    chore: add custom element function

--- src/index.js
+++ src/index.js
@@ -10,6 +10,27 @@ root.render(
   </React.StrictMode>
 );
 
+class PillarAPCharts extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = '<div id="pillarAPCharts"></div>';
+
+    const props = {
+      siteId: this.getAttribute('siteId'),
+    };
+
+    ReactDOM.render(
+      <App {...props} />,
+      document.querySelector('#pillarAPCharts')
+    );
+  }
+}
+
+if (customElements.get('pillar-ap-charts')) {
+  console.log('Skipping registration for <blog-editor> (already registered)');
+} else {
+  customElements.define('pillar-ap-charts', PillarAPCharts);
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
